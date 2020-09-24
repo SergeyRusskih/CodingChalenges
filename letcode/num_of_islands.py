ce6018d030915a8c99afe34a50ebccdf0e8555da
Sergei Russkikh <sergeivrusskikh@gmail.com>
2020-09-23 23:06:27 -0700

    Number of islands working

--- letcode/num_of_islands.py
+++ letcode/num_of_islands.py
@@ -1,26 +1,41 @@
-import queue
+import queue as queue
 
 def num_of_islands(grid):
-    visited = [[ False for x in enumerate(grid)    ]
-                       for y in enumerate(grid[0]) ]
     count = 0
     q = queue.Queue()
-    for i, row in enumerate(grid):
-        for j, cell in enumerate(row):
-            if visited[i][j] or cell == '0':
+    grid_len = len(grid)
+    row_len = 0
+    if grid_len > 0:
+        row_len = len(grid[0])
+    for i in range(grid_len):
+        for j in range(row_len):
+            if grid[i][j] == '0':
                 continue
             
             q.put((i, j))
             
             while q.qsize() != 0:
                 current_row, current_column = q.get()
-                if current_row + 1 < len(grid) and not visited[current_row + 1][current_column]:
-                    q.put((current_row + 1, current_column))
-                if current_column + 1 < len(row) and not visited[current_row][current_column + 1]:
-                    q.put((current_row, current_column + 1))
-
-                visited[current_row][current_column] = True
-            
+                next_row = current_row + 1
+                if next_row < grid_len:
+                    if grid[next_row][current_column] != '0':
+                        q.put((next_row, current_column))
+                        grid[next_row][current_column] = '0'
+                next_row = current_row - 1
+                if next_row >= 0:
+                    if grid[next_row][current_column] != '0':
+                        q.put((next_row, current_column))
+                        grid[next_row][current_column] = '0'
+                next_column = current_column + 1
+                if next_column < row_len:
+                    if grid[current_row][next_column] != '0':
+                        q.put((current_row, next_column))
+                        grid[current_row][next_column] = '0'
+                next_column = current_column - 1
+                if next_column >= 0:
+                    if grid[current_row][next_column] != '0':
+                        q.put((current_row, next_column))
+                        grid[current_row][next_column] = '0'
             count += 1
     return count
 
@@ -41,3 +56,11 @@ def test_example_2():
         ["0","0","0","1","1"]
     ]
     assert num_of_islands(grid) == 3
+    
+def test_example_3():
+    grid = [
+        ["1","1","1"],
+        ["0","1","0"],
+        ["1","1","1"]
+    ]
+    assert num_of_islands(grid) == 1
