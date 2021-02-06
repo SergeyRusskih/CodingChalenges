e4eb45cd281cba9b947928fdfb24d38e93707501
Sergei Russkikh <sergeivrusskikh@gmail.com>
2021-02-05 22:04:40 -0800

    longest awesome

---
+++ leetcode/hard/longest_awesome.py
@@ -0,0 +1,11 @@
+def longest_awesome(str):
+    pass
+
+def test_example_1():
+    longest_awesome("3242415") == 5
+
+def test_example_2():
+    longest_awesome("12345678") == 1
+
+def test_example_3():
+    longest_awesome("213123") == 6
