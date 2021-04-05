5a2812e9acfe1e6551e7958676676697565cfa56
Sergei Russkikh <sergeivrusskikh@gmail.com>
2021-04-04 18:10:28 -0700

    n queen

--- backtracking/n-queen.py
+++ backtracking/n-queen.py
@@ -3,36 +3,32 @@ def n_queen(n):
     sums = set()
     diffs = set()
 
-    def dfs(row, result):
+    boards = []
+    def dfs(row, board):
         if row == n:
-            return result
+            boards.append(list(board))
         
         for column in range(n):
 
             # https://www.geeksforgeeks.org/backtracking-introduction/
-            if (column - row) not in diffs and (column + row) not in sums and column not in result:
+            if (column - row) not in diffs and (column + row) not in sums and column not in board:
 
                 # apply value
                 sums.add(column + row)
                 diffs.add(column - row)
-                result.append(column)
+                board.append(column)
 
                 # find solution
-                board = dfs(row + 1, result)
-                if board:
-                    return board
+                dfs(row + 1, board)
 
                 # remove value
                 sums.remove(column + row)
                 diffs.remove(column - row)
-                result.pop()
-
-        return None
+                board.pop()
 
 
-    result = []
-    dfs(0, result)
-    return [ "." * column + "Q" + "." * (n-column-1) for column in result ]
+    dfs(0, [])
+    return [[ "." * column + "Q" + "." * (n-column-1) for column in board ] for board in boards ]
 
 def test_3_queen():
     result = n_queen(3)
@@ -41,8 +37,16 @@ def test_3_queen():
 def test_4_queen():
     result = n_queen(4)
     assert result == [
-        '.Q..',
-        '...Q',
-        'Q...',
-        '..Q. '
+        [
+            ".Q..",
+            "...Q",
+            "Q...",
+            "..Q."
+        ],
+        [
+            "..Q.",
+            "Q...",
+            "...Q",
+            ".Q.."
+        ]
     ]
