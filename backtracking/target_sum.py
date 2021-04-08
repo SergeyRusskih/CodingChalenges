f8f7ce49c718b764b5277414fa7d238df76293b6
Sergei Russkikh <sergeivrusskikh@gmail.com>
2021-04-07 21:03:40 -0700

    target sum dp

--- backtracking/target_sum.py
+++ backtracking/target_sum.py
@@ -14,7 +14,34 @@ def target_sum(arr, k):
     dfs(0, 0, [])
     return len(results)
 
+
+def target_sum_dp(arr, k):
+
+    count = 0
+    diffs = set([k])
+
+    while diffs:
+        current = diffs.pop()
+
+        index = 0
+        while index < len(arr):
+            diff = current - arr[index]
+            if diff == 0:
+                count += 1
+                arr.pop(index)
+            elif diff > 0 and diff not in diffs:
+                diffs.add(diff)
+
+            index += 1
+        
+    return count
+
 def test_target_sum():
     arr = [ 2, 3, 5, 6, 8 ]
     result = target_sum(arr, 7)
+    assert result == 2
+
+def test_target_sum_dp():
+    arr = [ 2, 3, 5, 6, 8 ]
+    result = target_sum_dp(arr, 7)
     assert result == 2
